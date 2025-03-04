732dc52bb25ab834fccec750f839d87cfda1509a
Junaid S Merchant, PhD <merchantjs@gmail.com>
2025-03-03 23:33:51 -0500

    Add files via upload

--- Shiny7Outcomes_wStateDivisions_20250303.R
+++ Shiny7Outcomes_wStateDivisions_20250303.R
@@ -7,6 +7,8 @@ library(ggExtra)
 
 df <- readr::read_csv("https://raw.githubusercontent.com/JunaidMerchant/MAIHDA_YRBSS/refs/heads/main/predict_probabilities/All7Outcomes_Strata40_wStateDivisions_reduced.csv")
 
+# df$Year=factor(df$Year)
+
 Questions=list("Suicidal Ideation: During the past 12 months, did you ever seriously consider attempting suicide? (YES/NO)",
                "Suicide Attempts: During the past 12 months, how many times did you actually attempt suicide?                (NOTE: transformed to binary by giving a YES for 1 or more suicide attempts; No for none.)",
                "Suicidal Planning: During the past 12 months, did you make a plan about how you would attempt suicide? (YES/NO)",
@@ -56,76 +58,159 @@ ui <- page_sidebar(
       selected = c(2017,2019,2021,2023),
       multiple = TRUE
     ),
+    input_switch("x", "x-axis = Year (vs Division)", value = TRUE, width = NULL),
+    input_switch("row", "Rows = Race (vs sex orient.)", value = TRUE, width = NULL),
     downloadButton('downloadData', 'Download Data')
   ),
   
   # Show a plot of the generated distribution
   htmlOutput('source'),
-  htmlOutput('question'),
-  plotOutput("line")
-    
+  plotOutput("line"),
+  htmlOutput('foot')
   
   
-  )
-  
+)
 
 
 
 
-  
+
+
 server <- function(input, output, session) {
   
-    subsetted <- reactive({
-      req(input$races, input$so,input$gender, input$years,input$Outcome,input$division)
-      df |> filter(Race %in% input$races) |> 
-        filter(Sexual_Orientation %in% input$so) |>
-        filter(Gender %in% input$gender) |>
-        filter(Year %in% input$years) |>
-        filter(outcome %in% input$Outcome) |>
-        filter(Geographic %in% input$division)
-    })
+  subsetted <- reactive({
+    req(input$races, input$so,input$gender, input$years,input$Outcome,input$division)
     
-    question <- observe({
-      output$question=renderText(c(Questions[[input$Outcome]]))
-    })
-    
-    
-    Cdc="<b>Question from Center for Disease Control's <a href='https://www.cdc.gov/yrbs/index.html'>Youth Risk Behavior Survey</a>:</b>"
-    
-    question <- observe({
-      output$question=renderText(paste0("<i>",Questions[[input$Outcome]],"</i>"))
-    })
-    
-    output$source=renderText(Cdc)
-    
-  # output$details=renderText(c("Predicted Probabilities are estimated using the I-MAIHDA (Intersectional Multilevel Analysis of Individual Heterogeneity and Discriminate Accuracy; see: https://pmc.ncbi.nlm.nih.gov/articles/PMC11059336/ )"))
-    
-  output$line <- renderPlot({
-    
-    p <- ggplot(subsetted(), aes(factor(Year), predicted,shape=Gender,color=Sexual_Orientation)) +
-      theme_light() +
-      facet_grid(vars(as.factor(Race)), vars(state)) +
-      theme(strip.text.y = element_text(size = 10,face="bold",color="black")) +
-      theme(strip.text.x = element_text(size = 10,face="bold",color="black")) +
-      theme(strip.background =element_rect(fill="white")) +
-      geom_line(aes(group=strata2, linetype=Gender)) +
-      # geom_point(size=.8) +
-      geom_pointrange(aes(ymin = conf.low, ymax = conf.high)) +
-      theme(axis.text.y=element_text(size=10,face="bold")) +
-      theme(axis.text.x = element_text(size=10,face="bold",angle=90,vjust = 1,hjust = 1)) +
-      scale_shape_manual(values = c(15, 16)) +
-      scale_linetype_manual(values=c( "longdash","solid"))+
-      xlab("Year") +
-      ylab("% YES (predicted probability)") +
-      scale_color_manual(values = c("red4","grey2","gold2","green4")) +
-      ggtitle(input$Outcome) +
-      theme(plot.title = element_text( size=16, face="bold",hjust=.5))
-    
-    
-    
-    p
-  }, res = 100)
+    df |> filter(Race %in% input$races) |> 
+      filter(Sexual_Orientation %in% input$so) |>
+      filter(Gender %in% input$gender) |>
+      filter(Year %in% input$years) |>
+      filter(outcome %in% input$Outcome) |>
+      filter(Geographic %in% input$division) 
+  })
   
+
+  source <- observe({
+    output$source=renderText(paste0("<p><b>Question from CDC's <a href='https://www.cdc.gov/yrbs/index.html'>Youth Risk Behavior Survey</a>:</b>"," ","<i>",Questions[[input$Outcome]],"</i></p>"))
+  })
+  
+  output$foot=renderText("<p>* Predicted probabilities estimated using <a href='https://doi.org/10.1016/j.ssmph.2024.101664'>I-MAIHDA</a>. LGBTQ laws from <a href='https://www.lgbtmap.org/'>MAP</a>. Racial sentiment from <a href='https://doi.org/10.1097/EDE.0000000000001671'>BD4HE</a>.</p>")
+  
+  
+  
+  line <- observe({
+    
+    if (input$x & input$row){
+      output$line <- renderPlot({
+        
+        p <- ggplot(subsetted(), aes(factor(Year), predicted,shape=Gender,color=Sexual_Orientation)) +
+          theme_light() +
+          facet_grid(vars(as.factor(Race)), vars(state)) +
+          theme(strip.text.y = element_text(size = 10,face="bold",color="black")) +
+          theme(strip.text.x = element_text(size = 10,face="bold",color="black")) +
+          theme(strip.background =element_rect(fill="white")) +
+          geom_line(aes(group=strata2, linetype=Gender)) +
+          # geom_point(size=.8) +
+          geom_pointrange(aes(ymin = conf.low, ymax = conf.high)) +
+          theme(axis.text.y=element_text(size=10,face="bold")) +
+          theme(axis.text.x = element_text(size=10,face="bold",angle=90,vjust = 1,hjust = 1)) +
+          scale_shape_manual(values = c(15, 16)) +
+          scale_linetype_manual(values=c( "longdash","solid"))+
+          xlab("Year") +
+          ylab("% YES (predicted probability)") +
+          scale_color_manual(values = c("red4","grey2","gold2","green4","skyblue3")) +
+          ggtitle(input$Outcome) +
+          theme(plot.title = element_text( size=16, face="bold",hjust=.5))
+        
+        
+        
+        p
+      }, res = 100)
+    } 
+    if (!input$x & input$row){
+      output$line <- renderPlot({
+        
+        p <- ggplot(subsetted(), aes(state, predicted,shape=Gender,color=Sexual_Orientation)) +
+          theme_light() +
+          facet_grid(vars(as.factor(Race)), vars(factor(Year))) +
+          theme(strip.text.y = element_text(size = 10,face="bold",color="black")) +
+          theme(strip.text.x = element_text(size = 10,face="bold",color="black")) +
+          theme(strip.background =element_rect(fill="white")) +
+          geom_line(aes(group=strata2, linetype=Gender)) +
+          # geom_point(size=.8) +
+          geom_pointrange(aes(ymin = conf.low, ymax = conf.high)) +
+          theme(axis.text.y=element_text(size=10,face="bold")) +
+          theme(axis.text.x = element_text(size=10,face="bold",angle=45,vjust = 1,hjust = 1)) +
+          scale_shape_manual(values = c(15, 16)) +
+          scale_linetype_manual(values=c( "longdash","solid"))+
+          xlab(input$division) +
+          ylab("% YES (predicted probability)") +
+          scale_color_manual(values = c("red4","grey2","gold2","green4","skyblue3")) +
+          ggtitle(input$Outcome) +
+          theme(plot.title = element_text( size=16, face="bold",hjust=.5))
+        
+        
+        
+        p
+      }, res = 100)
+      
+    } 
+    if (input$x & !input$row){
+      output$line <- renderPlot({
+        
+        p <- ggplot(subsetted(), aes(factor(Year), predicted,shape=Gender,color=as.factor(Race))) +
+          theme_light() +
+          facet_grid(vars(Sexual_Orientation), vars(state)) +
+          theme(strip.text.y = element_text(size = 10,face="bold",color="black")) +
+          theme(strip.text.x = element_text(size = 10,face="bold",color="black")) +
+          theme(strip.background =element_rect(fill="white")) +
+          geom_line(aes(group=strata, linetype=Gender)) +
+          # geom_point(size=.8) +
+          geom_pointrange(aes(ymin = conf.low, ymax = conf.high)) +
+          theme(axis.text.y=element_text(size=10,face="bold")) +
+          theme(axis.text.x = element_text(size=10,face="bold",angle=90,vjust = 1,hjust = 1)) +
+          scale_shape_manual(values = c(15, 16)) +
+          scale_linetype_manual(values=c( "longdash","solid"))+
+          xlab("Year") +
+          ylab("% YES (predicted probability)") +
+          scale_color_manual(values = c("skyblue3","grey2","orange3","darkgreen","red4","gold3","purple4")) +
+          ggtitle(input$Outcome) +
+          theme(plot.title = element_text( size=16, face="bold",hjust=.5))
+        
+        
+        
+        p
+      }, res = 100)
+    } 
+    if (!input$x & !input$row){
+      output$line <- renderPlot({
+        
+        p <- ggplot(subsetted(), aes(state, predicted,shape=Gender,color=as.factor(Race))) +
+          theme_light() +
+          facet_grid(vars(Sexual_Orientation), vars(factor(Year))) +
+          theme(strip.text.y = element_text(size = 10,face="bold",color="black")) +
+          theme(strip.text.x = element_text(size = 10,face="bold",color="black")) +
+          theme(strip.background =element_rect(fill="white")) +
+          geom_line(aes(group=strata, linetype=Gender)) +
+          # geom_point(size=.8) +
+          geom_pointrange(aes(ymin = conf.low, ymax = conf.high)) +
+          theme(axis.text.y=element_text(size=10,face="bold")) +
+          theme(axis.text.x = element_text(size=10,face="bold",angle=45,vjust = 1,hjust = 1)) +
+          scale_shape_manual(values = c(15, 16)) +
+          scale_linetype_manual(values=c( "longdash","solid"))+
+          xlab(input$division) +
+          ylab("% YES (predicted probability)") +
+          scale_color_manual(values = c("skyblue3","grey2","orange3","darkgreen","red4","gold3","purple4")) +
+          ggtitle(input$Outcome) +
+          theme(plot.title = element_text( size=16, face="bold",hjust=.5))
+        
+        
+        
+        p
+      }, res = 100)
+    }
+    
+  })
   
 }
 
